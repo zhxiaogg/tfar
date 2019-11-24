33046964460d683c3e303ef05a0383846e19885b
zhxiaog <zhxiaog@outlook.com>
2019-11-24 17:50:09 +0800

    add server states

--- src/states.rs
+++ src/states.rs
@@ -10,3 +10,14 @@ pub use persistent::Command;
 pub use persistent::LogEntry;
 pub use persistent::PersistentState;
 pub use persistent::TermId;
+
+/// The three states for servers in Raft cluter
+pub enum ServerState {
+    /// The one and only leader in cluster
+    Leader,
+    /// A server with an ongoing election
+    Candidate,
+    /// A server waiting for requests from other servers(vote, append entries 
+    /// and snapshot)
+    Follower,
+}
